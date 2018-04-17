619d2a859ea7ea81e007d2819fe3da9d8624726b
Wendy Segura <gwenevere05@gmail.com>
2018-04-17 20:44:58 +0000

    fixed log and created js and php for contact us

---
+++ docs/php/process_form.php
@@ -0,0 +1,54 @@
+<?php
+$errorMSG = "";
+// NAME
+if (empty($_POST["name"])) {
+    $errorMSG = "Name is required ";
+} else {
+    $name = $_POST["name"];
+}
+// EMAIL
+if (empty($_POST["email"])) {
+    $errorMSG .= "Email is required ";
+} else {
+    $email = $_POST["email"];
+}
+// SUBJECT
+if (empty($_POST["subject"])) {
+    $errorMSG .= "Subject is required ";
+} else {
+    $subject = $_POST["subject"];
+}
+// MESSAGE
+if (empty($_POST["message"])) {
+    $errorMSG .= "Message is required ";
+} else {
+    $message = $_POST["message"];
+}
+$EmailTo = "smartleasecontracts@gmail.com";
+// prepare email body text
+$Body = "";
+$Body .= "Name: ";
+$Body .= $name;
+$Body .= "\n";
+$Body .= "Email: ";
+$Body .= $email;
+$Body .= "\n";
+$Body .= "Subject: ";
+$Body .= $subject;
+$Body .= "\n";
+$Body .= "Message: ";
+$Body .= $message;
+$Body .= "\n";
+// send email
+$success = mail($EmailTo, $Body, "From:".$email);
+// redirect to success page
+if ($success && $errorMSG == ""){
+   echo "success";
+}else{
+    if($errorMSG == ""){
+        echo "Something went wrong :(";
+    } else {
+        echo $errorMSG;
+    }
+}
+?>
